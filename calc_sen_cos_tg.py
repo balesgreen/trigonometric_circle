a4a12773771239a42bef9dc4097f50590f46ec83
Mark662 <marktwainer2020@gmail.com>
2021-11-16 18:26:06 -0300

    Add files via upload

---
+++ calc_sen_cos_tg.py
@@ -0,0 +1,12 @@
+import math
+
+angulo = float(input("Informe um angulo: "))
+
+seno = math.sin(math.radians(angulo))
+print("O valor do seno de ", angulo, " é ", seno)
+
+cosseno = math.cos(math.radians(angulo))
+print("O valor do cosseno de ", angulo, " é ", cosseno)
+
+tangente = math.tan(math.radians(angulo))
+print("O valor da tangente de ", angulo, " é ", tangente)
